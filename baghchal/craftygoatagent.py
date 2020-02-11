c7d67e530a367e9e378172f3548f02bfa156c937
andrewcp54 <andrewcp540@gmail.com>
2020-02-11 14:45:54 -0700

    cleaned up code a bit, occams razor still dumb

--- baghchal/craftygoatagent.py
+++ baghchal/craftygoatagent.py
@@ -2,22 +2,13 @@ from agent import Agent
 from const import Const
 from game import Game
 from move import Move
-from typing import List
-import random
 
 class CraftyGoatAgent(Agent):
     def __init__(self, game : Game):
         super(CraftyGoatAgent, self).__init__(game,Const.MARK_GOAT)
     def propose(self) -> Move:  
         moves = self.game.goatMoves()
-        #print(moves[0])
+        # returns first possible place for goat to go, starting at a1 each time. 
+        # one way to make this faster, is if we know a goat has gone and been placed and NOT captured, remove that from list of moves
+        # pretty sure this would only affect computation speeds, not really benefit gameplay
         return moves[0]
-        '''captures : List[Move]=[]
-        for move in moves:
-            if move.capture:
-                captures.append(move)
-        if len(captures) != 0:
-            return random.choice(captures)
-        else:
-            return random.choice(moves)
-        '''
